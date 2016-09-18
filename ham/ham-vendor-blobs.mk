3f52d29eb3599d947bfb149b289d604dcf67869e
Ordenkrieger <OrdenKriegerEDR@googlemail.com>
2016-09-18 22:15:51 +0200

    ham: Add busybox

--- ham/ham-vendor-blobs.mk
+++ ham/ham-vendor-blobs.mk
@@ -16,7 +16,8 @@
 
 ifeq ($(QCPATH),)
 PRODUCT_COPY_FILES += \
-    vendor/zuk/ham/proprietary/bin/btnvtool:system/bin/btnvtool \
+    vendor/zuk/ham/proprietary/bin/busybox:system/bin/busybox \
+	vendor/zuk/ham/proprietary/bin/btnvtool:system/bin/btnvtool \
     vendor/zuk/ham/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
     vendor/zuk/ham/proprietary/vendor/lib/libbtnv.so:system/vendor/lib/libbtnv.so \
     vendor/zuk/ham/proprietary/bin/mpdecision:system/bin/mpdecision \
